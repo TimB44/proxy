b256fc74960119a75e8fd19cc0b211afab8bb62a
TimB44 <tblamires22@gmail.com>
2025-06-24 15:45:16 -0600

    testing

--- tests/client.py
+++ tests/client.py
@@ -2,7 +2,8 @@ from socket import socket, AF_INET, SOCK_STREAM
 
 with socket(AF_INET, SOCK_STREAM) as skt:
     skt.connect(("localhost", 2100))
-    skt.send(b"GET http://example.com/hi HTTP/1.0\r\n\r\n")
+    # skt.send(b"GET http://localhost:5500/proxy/cache/enable HTTP/1.0\r\n\r\n")
+    skt.send(b"GET http://localhost:5500/info.txt HTTP/1.0\r\n\r\n")
     response = bytearray()
     resp_part = skt.recv(2048)
     while len(resp_part) != 0:
